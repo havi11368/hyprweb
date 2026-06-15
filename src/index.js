35ef37cf53c10e78a2d5290b0897b051f3fa7fc5
Toast <85591146+havi11368@users.noreply.github.com>
2026-06-14 21:11:16 -0400

    add uv

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 function addWindow() {
     const window = document.createElement("div");
     window.id = "windowTab"
+    window.innerHTML = `
+    <iframe class="frame" src="./search.html"></iframe>
+    `
     document.querySelector("#windowContainer").appendChild(window);
 }
