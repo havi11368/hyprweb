18427bdacba6402600a7fcca3d5210608e5a2a7c
Toast <85591146+havi11368@users.noreply.github.com>
2026-06-15 16:35:35 -0400

    Add more CSS to the top bar

--- src/index.js
+++ src/index.js
@@ -43,3 +43,18 @@ async function setTransport() {
 
 }
 setTransport()
+
+//...Also taken from ACE
+
+function startTime() {
+  const today = new Date();
+  document.getElementById('time').innerHTML =  today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true})
+  setTimeout(startTime, 5000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+startTime();
